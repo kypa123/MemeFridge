eb868025c15bcedbc83f56cac929b4c6ac382776
주지호 <wallcraft@jujihoui-MacBookAir.local>
2023-04-18 00:56:32 +0900

    [추가]loadRankContent 함수 추가

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,10 +1,36 @@
 import * as Api from '../api.js';
 
-const container = document.getElementById('content-container');
-let offset = 0
+
+const container = document.getElementById('all-contents');
+const rankContainer = document.getElementById('rank-contents');
+
+async function loadRankContent(){
+    try{
+        const result = await Api.get('/contents','/rank');
+        console.log('main.js까지 무사히 도착완료.')
+        const row = document.createElement('div')
+        row.className = "row"
+        result.forEach(el=>{
+            row.innerHTML += `
+        <div class="content">
+            <a href="/detail/${el.id}">
+                <figure>
+                    <img class="content-img" src="${el.url}">
+                    <p>${el.title}</p>
+                </figure>    
+            </a>
+        </div>`
+        });
+        rankContainer.appendChild(row)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
 
 async function loadMainContent(){
     try{
+        let offset = 0
         while(true){
             const result = await Api.get('/contents',`?offset=${offset}`)
             const row = document.createElement('div')
@@ -30,5 +56,5 @@ async function loadMainContent(){
     }
 }
 
-
+loadRankContent()
 loadMainContent()
