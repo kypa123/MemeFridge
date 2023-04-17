0e7acdd6e517c6c2d3be2e2d474de639b000bd5c
주지호 <wallcraft@jujihoui-MacBookAir.local>
2023-04-18 00:54:11 +0900

    [추가]content/rank router 추가, 기존의 /:id를 /id?id=query 구조로 변경

--- src/routes/content-router.js
+++ src/routes/content-router.js
@@ -15,9 +15,19 @@ contentRouter.get('/', async function(req, res, next){
     }
 });
 
-contentRouter.get('/:id', async function(req, res, next){
+contentRouter.get('/rank', async function(req,res,next){
     try{
-        const id = req.params.id;
+        const result = await contentService.getCacheRankData();
+        res.json(result)
+    }
+    catch(err){
+        next(err)
+    }
+});
+
+contentRouter.get('/id', async function(req, res, next){
+    try{
+        const id = req.query.id;
         const result = await contentService.findByContentId(id);
         console.log(result.rows[0]);
         res.json(result.rows[0]);
@@ -26,6 +36,8 @@ contentRouter.get('/:id', async function(req, res, next){
         next(err)
     }
 });
+
+
 
 contentRouter.post('/', upload, async function(req, res, next){
     try{
