b7c5dacddbd2b21d41c2af2e4b9b917f883756d3
Jiho Joo <aksjajsxm123@gmail.com>
2023-05-04 05:30:04 +0900

    [추가] 회원가입 뷰 등록

--- src/routes/views-router.js
+++ src/routes/views-router.js
@@ -8,15 +8,15 @@ const viewsRouter = express.Router();
 
 viewsRouter.use('/', serveStatic('home'));
 viewsRouter.use('/nav',serveStatic('nav'));
-viewsRouter.use('/footer',serveStatic('footer'))
-viewsRouter.use('/content-modal',serveStatic('content-modal'))
+viewsRouter.use('/footer',serveStatic('footer'));
+viewsRouter.use('/content-modal',serveStatic('content-modal'));
 viewsRouter.use('/main', serveStatic('main'));
-viewsRouter.use('/images', serveStatic('images'))
-viewsRouter.use('/detail/:id', serveStatic('detail'))
+viewsRouter.use('/images', serveStatic('images'));
+viewsRouter.use('/detail/:id', serveStatic('detail'));
 viewsRouter.use('/register', serveStatic('register'));
+viewsRouter.use('/sign-up', serveStatic('sign-up'));
 
-
-viewsRouter.use('/',serveStatic(''))
+viewsRouter.use('/',serveStatic(''));
 
 function serveStatic(resource) {
     const resourcePath = path.join(__dirname, `../views/${resource}`);
