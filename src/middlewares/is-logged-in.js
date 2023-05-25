246532f2a959839cf6687ef4eeaf2d91fc825a8d
Jiho Joo <aksjajsxm123@gmail.com>
2023-05-25 16:17:06 +0900

    [수정]req.cookies.token 유무에 따라 res, next로 분리

--- src/middlewares/is-logged-in.js
+++ src/middlewares/is-logged-in.js
@@ -2,28 +2,29 @@ import jwt from 'jsonwebtoken';
 
 async function isLoggedIn(req, res, next) {
     console.log('미들웨어')
-    const token = req.cookies.token;
-    if (!token) {
-        res.status(403).json({
+    if (!req.cookies.token) {
+        res.json({
             status: 'error',
             statusCode: 403,
             message: '로그인되어 있지 않습니다!',
         });
     }
-    
-    try{
-        const tokenInfo = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        console.log('정상적으로 토큰을 인식하였습니다')
-        console.log(tokenInfo)
-        req.tokenInfo = tokenInfo;
-        next();
-    }
-    catch(err){
-            req.tokenInfo = err.message
-            console.log('토큰이 만료됐거나 문제가있습니다.')
-            console.log(err.message)
+    else{
+        try{
+            const token = req.cookies.token;
+            const tokenInfo = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            console.log('정상적으로 토큰을 인식하였습니다')
+            console.log(tokenInfo)
+            req.tokenInfo = tokenInfo;
             next();
         }
+        catch(err){
+                req.tokenInfo = err.message
+                console.log('토큰이 만료됐거나 문제가있습니다.')
+                console.log(err.message)
+                next();
+            }
+    }
 }
 
 export default isLoggedIn;
