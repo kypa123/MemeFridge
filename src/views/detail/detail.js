d66f7d3611fe7c5725776957ee3568e3344adb77
Jiho Joo <aksjajsxm123@gmail.com>
2023-06-13 07:05:59 +0900

    [추가]detail 하단에 조회수 추가

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -4,13 +4,14 @@ import * as Api from '/api.js';
 const desc = document.getElementById('detail-desc');
 const img = document.getElementById('detail-img')
 const title = document.getElementById('detail-title');
+const view = document.getElementById('content-view');
+
 
 async function dataInsert(){
     try{
         const contentId = location.pathname.split('/')[2];
         const result = await Api.get('/contents/id',`?id=${contentId}`);
         if (result){
-            console.log(result)
             title.innerText = result.title || result.tag
             img.src = result.url;
             const tags = result.tag.split(" ")
@@ -20,6 +21,7 @@ async function dataInsert(){
                 span.innerText = t;
                 desc.appendChild(span);
             })
+            view.innerText = result.count;
         }
     }
     catch(err){
