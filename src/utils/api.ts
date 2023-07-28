31cd43893393a9c0e6e246f28d942d0c5bc021e4
Jiho Joo <aksjajsxm123@gmail.com>
2023-07-28 21:39:42 +0900

    [추가]server side fetch를 위한 node-fetch dependency 추가, url에 http 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,17 @@
-async function get(endpoint: string, params: string = '') {
-    const apiUrl = `${endpoint}/${params}`;
-    const res = await fetch(apiUrl);
-    return await res.json();
+import fetch from 'node-fetch';
+
+async function get(endpoint: string, params: string = ''): Promise<any> {
+    try {
+        const apiURL = `http://${endpoint}/${params}`;
+        const res = await fetch(apiURL);
+        return await res.json();
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 async function post(endpoint: string) {
-    const apiUrl = endpoint;
+    const apiUrl = 'http://' + endpoint;
     const res = await fetch(apiUrl, {
         method: 'POST',
     });
