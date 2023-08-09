c07b715bce3084b246c724dc8ff6870fdc7f0163
Jiho Joo <aksjajsxm123@gmail.com>
2023-08-09 23:42:35 +0900

    [추가]contentRouter, buzzwordRouter 통합테스트 추가

--- test/routes/router.test.ts
+++ test/routes/router.test.ts
@@ -5,6 +5,8 @@ import connectBusboy from 'connect-busboy';
 import userRouter from '../../src/routes/user-router.ts';
 import contentRouter from '../../src/routes/content-router.ts';
 import buzzwordRouter from '../../src/routes/buzzword-router.ts';
+import upload from '../../src/middlewares/image-upload.ts';
+import errorHandler from '../../src/middlewares/errorHandler.ts';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,8 +16,8 @@ app.use(connectBusboy());
 app.use('/users', userRouter);
 app.use('/contents', contentRouter);
 app.use('/buzzwords', buzzwordRouter);
-
-//userRouter, controller test
+app.use(errorHandler);
+// userRouter, controller test
 describe('GET /users', () => {
     it('이름으로 유저 가져오기', done => {
         request(app)
@@ -177,3 +179,249 @@ describe('DELETE /users', () => {
             .expect({ status: 200, message: 'ok' }, done);
     });
 });
+
+describe('POST /users/logout', () => {
+    it('쿠키에 저장된 token값을 삭제한다', async () => {
+        const response = await request(app).post('/users/logout');
+        const clearCookieSpy = jest.spyOn(response.res, 'clearCookie');
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'ok' });
+        expect(clearCookieSpy).toHaveBeenCalledWith('token');
+    });
+});
+
+//contentRouter, controller test
+describe('GET /contents', () => {
+    it('offset을 통해 4개의 데이터를 가져온다', async () => {
+        const response = await request(app).get('/contents?offset=2');
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('rowCount');
+        expect(response.body.rowCount).toEqual(4);
+    });
+    it('데이터 개수를 넘어서는 offset이라면, rowCount가 0으로 반환된다', async () => {
+        const response = await request(app).get('/contents?offset=9999');
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('rowCount');
+        expect(response.body.rowCount).toEqual(0);
+    });
+});
+
+describe('GET /contents/rank/zzal', () => {
+    it('가장 높은 조회수의 컨텐츠 4개가 배열에 담겨 반환된다', done => {
+        request(app)
+            .get('/contents/rank/zzal')
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toHaveLength(4);
+                expect(Array.isArray(res.body)).toBe(true);
+            })
+            .end(done);
+    });
+});
+
+describe('GET /contents/rank/tags', () => {
+    it('공백을 기준으로 여러개의 태그를 담은 string을 반환한다', done => {
+        request(app)
+            .get('/contents/rank/tags')
+            .expect(200)
+            .expect(res => {
+                expect(typeof res.body).toBe('string');
+            })
+            .end(done);
+    });
+});
+describe('GET /contents/id', () => {
+    it('존재하지 않는 컨텐츠 아이디라면, 에러를 반환한다', done => {
+        request(app)
+            .get('/contents/id?id=2')
+            .expect(500)
+            .expect(res => {
+                expect(res.text).toContain(
+                    '에러가 발생했습니다. 코드 : undefined',
+                );
+            })
+            .end(done);
+    });
+    it('올바른 컨텐츠 아이디라면, 짤 컨텐츠를 반환한다', done => {
+        request(app)
+            .get('/contents/id?id=6')
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toHaveProperty('id');
+                expect(res.body).toHaveProperty('creator');
+                expect(res.body).toHaveProperty('title');
+                expect(res.body).toHaveProperty('tags');
+                expect(res.body).toHaveProperty('count');
+                expect(res.body).toHaveProperty('url');
+                expect(res.body).toHaveProperty('created_at');
+            })
+            .end(done);
+    });
+});
+describe('GET /contents/user', () => {
+    it('존재하지 않는 아이디라면, 에러를 반환한다', done => {
+        request(app)
+            .get('/contents/user?user=asdf')
+            .expect(500)
+            .expect(res => {
+                expect(res.text).toContain(
+                    '에러가 발생했습니다. 코드 : undefined',
+                );
+            })
+            .end(done);
+    });
+    it('존재하는 아이디라면, 쿼리 결과문을 반환한다', done => {
+        request(app)
+            .get('/contents/user?user=testuser')
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toHaveProperty('rowCount');
+                expect(res.body).toHaveProperty('rows');
+            })
+            .end(done);
+    });
+});
+describe('GET /contents/tags', () => {
+    it('검색결과가 있다면, 결과를 담은 쿼리 결과문을 반환한다', done => {
+        request(app)
+            .get(encodeURI('/contents/tags?tags=동물'))
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toHaveProperty('rowCount');
+                expect(res.body.rowCount > 0).toBe(true);
+                expect(res.body).toHaveProperty('rows');
+            })
+            .end(done);
+    });
+    it('검색결과가 없다면, 빈 결과를 담은 쿼리 결과문을 반환한다', done => {
+        request(app)
+            .get('/contents/tags?tags=1234')
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toHaveProperty('rowCount');
+                expect(res.body.rowCount > 0).toBe(false);
+                expect(res.body).toHaveProperty('rows');
+            })
+            .end(done);
+    });
+});
+
+// describe('POST /contents', () => {
+//     //업로드 함수 모킹
+//     const spyField = jest
+//         .spyOn(upload.prototype, 'on')
+//         .mockImplementation((event, callback: any) => {
+//             if (event === 'file') {
+//                 const testFile = { pipe: jest.fn() }; // 모킹된 pipe 함수
+//                 callback('fieldname', testFile, {});
+//             }
+//         });
+//     it('이미지를 업로드하고 값을 반환한다', done => {
+//         request(app)
+//             .post('/contents')
+//             .field('name', 'testname')
+//             .field('tag', 'testTag')
+//             .field('uploaderName', 'testuser')
+//             .field('uploaderPassword', 'testpwd')
+//             .expect(200)
+//             .expect(res => {
+//                 console.log('res:', res);
+//             })
+//             .end(done);
+//     });
+// });
+// describe('DELETE /contents', () => {});
+
+//buzzwordRouter, Controller test
+
+describe('GET /buzzwords', () => {
+    it('offset을 통해 4개의 유행어 데이터를 가져온다', done => {
+        request(app)
+            .get('/buzzwords?offset=2')
+            .expect(response => {
+                expect(response.status).toBe(200);
+                expect(response.body).toHaveProperty('rowCount');
+                expect(response.body.rowCount).toEqual(4);
+            })
+            .end(done);
+    });
+});
+describe('GET /buzzwords/id', () => {
+    it('존재하지 않는 id라면, rowCount가 0인채로 반환된다', done => {
+        request(app)
+            .get('/buzzwords/id?id=2')
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toHaveProperty('rowCount');
+                expect(res.body.rowCount).toEqual(0);
+            })
+            .end(done);
+    });
+    it('존재하는 id라면, 해당 데이터를 반환한다', done => {
+        request(app)
+            .get('/buzzwords/id?id=108')
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toHaveProperty('rowCount');
+                expect(res.body);
+                expect(res.body.rowCount > 0).toBe(true);
+                const data = res.body.rows[0];
+                expect(data).toHaveProperty('id');
+                expect(data).toHaveProperty('creator');
+                expect(data).toHaveProperty('name');
+                expect(data).toHaveProperty('description');
+                expect(data).toHaveProperty('tags');
+            })
+            .end(done);
+    });
+});
+describe('GET /buzzwords/user', () => {
+    it('존재하지 않는 아이디라면, rowCount가 0인 결과를 반환한다', done => {
+        request(app)
+            .get('/buzzwords/user?user=asdf')
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toHaveProperty('rowCount');
+                expect(res.body.rowCount).toEqual(0);
+            })
+            .end(done);
+    });
+
+    it('존재하는 아이디라면, 쿼리 결과문을 반환한다', done => {
+        request(app)
+            .get('/buzzwords/user?user=chatGPT')
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toHaveProperty('rowCount');
+                expect(res.body.rowCount > 0).toBe(true);
+                const data = res.body.rows[0];
+                expect(data).toHaveProperty('id');
+                expect(data).toHaveProperty('creator');
+                expect(data).toHaveProperty('name');
+                expect(data).toHaveProperty('description');
+                expect(data).toHaveProperty('tags');
+            })
+            .end(done);
+    });
+});
+describe('GET /buzzwords/tags', () => {
+    it('공백을 기준으로 여러개의 태그를 입력하고, 결과값을 반환한다', done => {
+        request(app)
+            .get('/buzzwords/tags?tags=testtag')
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toHaveProperty('rowCount');
+                expect(res.body.rowCount > 0).toBe(true);
+                const data = res.body.rows[0];
+                expect(data).toHaveProperty('id');
+                expect(data).toHaveProperty('creator');
+                expect(data).toHaveProperty('name');
+                expect(data).toHaveProperty('description');
+                expect(data).toHaveProperty('tags');
+            })
+            .end(done);
+    });
+});
+// describe('POST /buzzwords/dataAPI', () => {});
+// describe('POST /buzzwords', () => {});
+// describe('DELETE /buzzwords', () => {});
